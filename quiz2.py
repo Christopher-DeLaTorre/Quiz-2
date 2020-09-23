2b1704b51627df26fdab61d0594be7bc2a0d999a
Christopher-DeLaTorre <69987322+Christopher-DeLaTorre@users.noreply.github.com>
2020-09-23 10:18:26 -0700

    Add files via upload

---
+++ quiz2.py
@@ -0,0 +1,13 @@
+def get_animal(sound):
+    if sound == "meow":
+        result = "cat"
+        return result
+    elif sound == "woof":
+        result = "dog"
+        return result
+    elif sound == "quack":
+        result = "duck"
+        return result
+    else:
+        return "I don't recognize that sound."
+        
